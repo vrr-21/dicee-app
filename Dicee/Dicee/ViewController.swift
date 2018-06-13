ef7c56c7500d338b1b2ee72f2459f3fd3f3ae42f
vrr-21 <varun21exps@gmail.com>
2018-06-13 08:23:09 +0530

    Swift code added for random number generation. Outlets and actions connected to storyboard.

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+//    IBOutlet stands for 'Interface Builder Outlet'
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // What should happen when the button is pressed.
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        print(randomDiceIndex1)
+    }
+    
 
 }
 
