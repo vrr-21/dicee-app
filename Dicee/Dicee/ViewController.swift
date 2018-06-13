d519fec604124c4f6aca63d70cf3fe3618ab9ad8
vrr-21 <varun21exps@gmail.com>
2018-06-13 08:44:53 +0530

    Randomisation of images added. Dice works correct on pressing the roll button.

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
-    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     @IBOutlet weak var diceImageView1: UIImageView!
 //    IBOutlet stands for 'Interface Builder Outlet'
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -31,7 +31,9 @@ class ViewController: UIViewController {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
-        print(randomDiceIndex1)
+//        print(randomDiceIndex1)
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 
